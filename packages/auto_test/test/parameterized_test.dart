ac5ab9778c999b52d29873d4d1f6adb31c5fbbb2
Minsu Lee <amond@amond.net>
2022-02-21 14:17:01 +0900

    test: parameterized test

---
+++ packages/auto_test/test/parameterized_test.dart
@@ -0,0 +1,25 @@
+import 'package:auto_test/auto_test.dart';
+import 'package:test/test.dart';
+
+void main() {
+  defineAutoSuite(() {
+    defineAutoTests(ParameterizedTextExample);
+  }, name: 'ParameterizedTextExample!');
+}
+
+@autoTests
+class ParameterizedTextExample {
+  @autoTest
+  @autoSource
+  void parameterizedTest(int a, int b) {
+    Calculator sut = Calculator();
+    int actual = sut.add(a, b);
+    expect(a + b, actual);
+  }
+}
+
+class Calculator {
+  int add(int a, int b) {
+    return a + b;
+  }
+}
